29024dbdfaf5ae2207858cb26208d44deb0dff1b
Charith Wickramasinghe <pvmcharith@gmail.com>
2024-12-15 02:17:19 +0530

    add contact page

--- api/main.py
+++ api/main.py
@@ -1,10 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def Home():
     return "Hello Flask from Vercel"
 
+
 @app.route("/about")
 def About():
     return "About page"
+
+
+@app.route("/contact")
+def About():
+    return "Contact page"
